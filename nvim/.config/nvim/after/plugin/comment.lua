1191918c87df0441ca786e455b9bab92729ce87f
darcy <darcy3057@gmail.com>
2023-07-24 21:41:44 +1000

    fix comments in nvim

--- nvim/.config/nvim/after/plugin/comment.lua
+++ nvim/.config/nvim/after/plugin/comment.lua
@@ -1,4 +1,21 @@
-vim.keymap.set("n", "<C-_>", function()
-    require('Comment.api').toggle.linewise.current()
-end, { noremap = true, silent = true })
+local api = require("Comment.api")
+
+-- Comment line in normal mode
+vim.keymap.set("n", "<C-_>", api.toggle.linewise.current)
+
+-- Comment line in insert mode
+vim.keymap.set("i", "<C-_>", function()
+    api.toggle.linewise.current()
+end)
+
+-- idk?
+local esc = vim.api.nvim_replace_termcodes(
+    "<ESC>", true, false, true
+)
+
+-- Comment line in visual mode
+vim.keymap.set("x", "<C-_>", function()
+    vim.api.nvim_feedkeys(esc, "nx", false)
+    api.toggle.linewise(vim.fn.visualmode())
+end)
 
