35db4ce9cd9a4c107a188b4c8755d1efd3e66c6b
darcy <darcy3057@gmail.com>
2023-08-08 21:36:48 +1000

    nvim - install only takes 2 opens of nvim (hopefully)

--- nvim/.config/nvim/lua/core/packer.lua
+++ nvim/.config/nvim/lua/core/packer.lua
@@ -2,6 +2,7 @@ local ensure_packer = function()
     local fn = vim.fn
     local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
     if fn.empty(fn.glob(install_path)) > 0 then
+        print("Installing packer...")
         fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
         vim.cmd [[packadd packer.nvim]]
         return true
@@ -20,7 +21,6 @@ return require("packer").startup(function(use)
     }
 
     use({ "rose-pine/neovim", as = "rose-pine" })
-    vim.cmd("colorscheme rose-pine")
 
     -- Treesitter
     use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
