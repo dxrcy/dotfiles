052096dca5f8cbf68ab5a1c8cf43135d916c4f6c
darcy <acednes@gmail.com>
2023-09-24 19:10:24 +1000

    nvim - idk

--- nvim/.config/nvim/after/plugin/lspconfig.lua
+++ nvim/.config/nvim/after/plugin/lspconfig.lua
@@ -1,10 +1,10 @@
-require("lspconfig").lua.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {"vim"},
-      },
-    },
-  },
-}
+-- require("lspconfig").lua.setup {
+--   settings = {
+--     Lua = {
+--       diagnostics = {
+--         -- Get the language server to recognize the `vim` global
+--         globals = {"vim"},
+--       },
+--     },
+--   },
+-- }
