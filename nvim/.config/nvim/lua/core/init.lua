860f4c28b59d008308e1b2c317ba0c00340a6a39
darcy <acednes@gmail.com>
2023-10-19 22:07:06 +1100

    nvim - cmdheight

--- nvim/.config/nvim/lua/core/init.lua
+++ nvim/.config/nvim/lua/core/init.lua
@@ -31,7 +31,7 @@ vim.opt.scrolloff = 8
 -- Disable 'signcolumn' - gutter left of line numbers
 vim.opt.signcolumn = 'no'
 -- Use same line for status line (lualine), commands, command output
-vim.opt.cmdheight = 0
+-- vim.opt.cmdheight = 0
 
 -- Disable 'Warning: changing a readonly file'
 vim.cmd("au BufEnter * set noro")
