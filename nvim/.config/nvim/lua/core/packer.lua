0cc0b3123fa027adec4db5572d39c18dc7db5deb
darcy <acednes@gmail.com>
2023-11-13 17:22:52 +1100

    nvim - fix nvim-cmp by adding vim-vsnip dependency

--- nvim/.config/nvim/lua/core/packer.lua
+++ nvim/.config/nvim/lua/core/packer.lua
@@ -53,6 +53,7 @@ return require("packer").startup(function(use)
             { "hrsh7th/nvim-cmp" },                  -- Required
             { "hrsh7th/cmp-nvim-lsp" },              -- Required
             { "L3MON4D3/LuaSnip" },                  -- Required
+            { "hrsh7th/vim-vsnip" },
         }
     }
 
