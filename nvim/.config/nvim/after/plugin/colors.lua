71866a51fc7c91cb8cb822456fac010862b6184c
darcy <acednes@gmail.com>
2024-01-23 21:50:47 +1100

    nvim - fix multi-window transparency

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -2,9 +2,12 @@ function Color(color)
     -- Color scheme
     color = color or "rose-pine"
     vim.cmd.colorscheme(color)
+
     -- Transparent Background
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
 end
 
 Color()
