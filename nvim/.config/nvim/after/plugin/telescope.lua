1573fe114f1adf9a7a8cd22adf82485032c0a22f
darcy <acednes@gmail.com>
2024-01-23 22:18:48 +1100

    nvim - telescope ignore files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -3,3 +3,17 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require("telescope").setup({
+    defaults = {
+        file_ignore_patterns = {
+            -- Build and module directories
+            "build", "target", "node_modules",
+            -- Object files and the like
+            "*.o", "*.so", "*.hi",
+            -- Lock files
+            "*.lock",
+        },
+    },
+})
+
