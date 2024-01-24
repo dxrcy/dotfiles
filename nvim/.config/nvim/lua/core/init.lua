63580302a57a3d7577395b2b329a8c8346e0573e
darcy <acednes@gmail.com>
2024-01-24 14:15:20 +1100

    nvim - revert undo history

--- nvim/.config/nvim/lua/core/init.lua
+++ nvim/.config/nvim/lua/core/init.lua
@@ -14,6 +14,12 @@ vim.opt.relativenumber = true
 -- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- Backup with undotree
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim-undodir"
+vim.opt.undofile = true
+
 -- Minimum lines to show ahead while scrolling
 vim.opt.scrolloff = 8
 
