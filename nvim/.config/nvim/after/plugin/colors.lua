35372a3c8ba121d2c8b9935be2738ed3bc96a002
darcy <acednes@gmail.com>
2024-01-29 16:02:50 +1100

    nvim - format

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -4,11 +4,11 @@ function Color(color)
     vim.cmd.colorscheme(color)
 
     -- Transparent Background
-    vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     -- For unfocused windows
-    vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalSB",    { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
     -- For gitsigns plugin
     -- ?? Makes gitsigns text color white
     -- vim.cmd([[highlight SignColumn     guibg=NONE]])
