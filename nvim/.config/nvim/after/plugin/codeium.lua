47ef79b11778eff05e55980d4fc5e14c2b1d9158
darcy <acednes@gmail.com>
2024-03-18 08:36:34 +1100

    nvim - codeium toggle keybind

--- nvim/.config/nvim/after/plugin/codeium.lua
+++ nvim/.config/nvim/after/plugin/codeium.lua
@@ -1,4 +1,6 @@
-vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-vim.keymap.set('i', '<C-N>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-vim.keymap.set('i', '<C-P>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
+vim.keymap.set("i", "<C-N>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true, silent = true })
+vim.keymap.set("i", "<C-P>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true, silent = true })
+vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
+
+vim.keymap.set("n", "<leader>ct", function() return vim.fn["CodeiumToggle"]() end, { expr = true, silent = true })
