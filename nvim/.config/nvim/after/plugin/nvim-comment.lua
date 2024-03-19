653155e8531ccb5e53cfc785ae41c8cfb980f455
darcy <acednes@gmail.com>
2024-03-19 13:21:56 +1100

    nvim - gleam commentstring

--- nvim/.config/nvim/after/plugin/nvim-comment.lua
+++ nvim/.config/nvim/after/plugin/nvim-comment.lua
@@ -3,7 +3,12 @@ require("nvim_comment").setup {
     comment_empty = true,
     comment_empty_trim_whitespace = true,
     create_mappings = false, -- manual keymaps
-    hook = nil
+
+    hook = function()
+        if vim.api.nvim_buf_get_option(0, "filetype") == "gleam" then
+            vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+        end
+    end
 }
 
 vim.keymap.set("n", "<C-_>", ":CommentToggle<CR>")
