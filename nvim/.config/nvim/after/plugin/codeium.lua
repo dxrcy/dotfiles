5fa552b2b08df398f5e86525d2b134d511fcd20e
darcy <acednes@gmail.com>
2024-03-23 09:27:52 +1100

    nvim - fix codeium bindings

--- nvim/.config/nvim/after/plugin/codeium.lua
+++ nvim/.config/nvim/after/plugin/codeium.lua
@@ -1,8 +1,16 @@
-vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
-vim.keymap.set("i", "<C-N>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true, silent = true })
-vim.keymap.set("i", "<C-P>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true, silent = true })
-vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
+-- Disable default bindings
+-- Stops <Tab> being always active
+vim.g.codeium_disable_bindings = 1
 
+-- Accept completion
+vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
+-- Cancel preview
+vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
+-- Cycle preview
+vim.keymap.set("i", "<C-j>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true, silent = true })
+vim.keymap.set("i", "<C-k>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true, silent = true })
+
+-- Toggle
 vim.keymap.set("n", "<leader>ct", function()
     vim.fn["CodeiumToggle"]()
     print("Toggled code completion")
