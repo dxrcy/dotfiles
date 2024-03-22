96d9e96595d3d7d09d37cc788fba79a1ead59954
darcy <acednes@gmail.com>
2024-03-22 20:30:44 +1100

    nvim - codeium toggle message

--- nvim/.config/nvim/after/plugin/codeium.lua
+++ nvim/.config/nvim/after/plugin/codeium.lua
@@ -3,4 +3,7 @@ vim.keymap.set("i", "<C-N>", function() return vim.fn["codeium#CycleCompletions"
 vim.keymap.set("i", "<C-P>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true, silent = true })
 vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
 
-vim.keymap.set("n", "<leader>ct", function() return vim.fn["CodeiumToggle"]() end, { expr = true, silent = true })
+vim.keymap.set("n", "<leader>ct", function()
+    vim.fn["CodeiumToggle"]()
+    print("Toggled code completion")
+end)
