45d7479084fb0a82bd17c68a2bdbc2bdfee15353
darcy <acednes@gmail.com>
2024-04-03 20:59:02 +1100

    nvim - remove cmp binding for crates

--- nvim/.config/nvim/after/plugin/nvim-cmp.lua
+++ nvim/.config/nvim/after/plugin/nvim-cmp.lua
@@ -22,11 +22,3 @@ cmp.setup({
         end, { "i", "s" }),
     }),
 })
-
-vim.api.nvim_create_autocmd("BufRead", {
-    group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-    pattern = "Cargo.toml",
-    callback = function()
-        cmp.setup.buffer({ sources = { { name = "crates" } } })
-    end,
-})
