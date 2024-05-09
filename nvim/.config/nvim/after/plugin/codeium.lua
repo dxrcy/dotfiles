451209b6c09ec5fa139b1b402082cc7e7f28159f
darcy <acednes@gmail.com>
2024-05-10 07:32:18 +1000

    nvim - change codeium keybind

--- nvim/.config/nvim/after/plugin/codeium.lua
+++ nvim/.config/nvim/after/plugin/codeium.lua
@@ -5,7 +5,7 @@ vim.g.codeium_disable_bindings = 1
 -- Accept completion
 vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
 -- Cancel preview
-vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
+vim.keymap.set("i", "<C-c>", function() return vim.fn["codeium#Clear"]() end, { expr = true, silent = true })
 -- Cycle preview
 vim.keymap.set("i", "<C-j>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true, silent = true })
 vim.keymap.set("i", "<C-k>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true, silent = true })
