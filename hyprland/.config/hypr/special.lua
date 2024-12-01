ec6b2071c9bb3fc0f2c264fd5a6a7ba1777bdd24
darcy <acednes@gmail.com>
2024-12-01 17:50:02 +1100

    hyprland: fix(sw): disable ferdium autostart

--- hyprland/.config/hypr/special.lua
+++ hyprland/.config/hypr/special.lua
@@ -22,7 +22,7 @@ Programs = {
         name = "social",
         class = "Ferdium",
         command = "ferdium",
-        autostart = true,
+        autostart = false,
     },
     {
         name = "vpn",
