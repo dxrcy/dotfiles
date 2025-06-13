86a67253aecfd2b76dfa853af80bdf189d25fd4e
darcy <acednes@gmail.com>
2025-06-13 10:30:55 +1000

    nvim: feat(lsp/java): use java version 21

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -2,10 +2,10 @@ local config = {
     cmd = {
         "/usr/bin/jdtls",
         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+        "--java-executable", "/usr/lib/jvm/java-21-openjdk/bin/java"
     },
 
     root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
 
 require("jdtls").start_or_attach(config)
-
