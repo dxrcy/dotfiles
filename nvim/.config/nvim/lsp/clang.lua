7753d099503b3753a3f15bab822a0ed8ca01554c
darcy <acednes@gmail.com>
2025-09-26 17:42:36 +1000

    nvim: chore: format `clang.lua`

--- nvim/.config/nvim/lsp/clang.lua
+++ nvim/.config/nvim/lsp/clang.lua
@@ -1,19 +1,19 @@
 return {
-	cmd = {
-		"clangd",
-		"--clang-tidy",
-		"--background-index",
-	},
-	filetypes = {
-		"c",
-		"cpp",
-		"h",
-		"hpp",
-		"cc",
-	},
-	root_markers = {
-		".git",
-		"compile_commands.json",
-		"compile_flags.txt",
-	},
+    cmd = {
+        "clangd",
+        "--clang-tidy",
+        "--background-index",
+    },
+    filetypes = {
+        "c",
+        "cpp",
+        "h",
+        "hpp",
+        "cc",
+    },
+    root_markers = {
+        ".git",
+        "compile_commands.json",
+        "compile_flags.txt",
+    },
 }
