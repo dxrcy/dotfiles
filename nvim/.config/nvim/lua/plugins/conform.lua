6c6887aa2d3963a912f118eb0ae478ed7d73450d
darcy <acednes@gmail.com>
2025-09-26 12:43:45 +1000

    nvim: feat: add `conform` for formatting

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+    { -- Autoformat
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
+        keys = {
+            {
+                "<leader>f",
+                function()
+                    require("conform").format({ async = true, lsp_format = "fallback" })
+                end,
+                mode = "",
+                desc = "[F]ormat buffer",
+            },
+        },
+        opts = {
+            notify_on_error = false,
+        },
+    },
+}
