38b7a253936c1501fbd096add625f07a6919ca1a
darcy <acednes@gmail.com>
2025-09-29 16:49:53 +1000

    nvim: feat: add `nvim-scrollbar` plugin

---
+++ nvim/.config/nvim/lua/plugins/scrollbar.lua
@@ -0,0 +1,15 @@
+return {
+    { -- Extensible scrollbar
+        "petertriho/nvim-scrollbar",
+        config = function()
+            require("scrollbar").setup({
+                handlers = {
+                    cursor = true,
+                    diagnostic = true,
+                    gitsigns = true,
+                    handle = true,
+                },
+            })
+        end,
+    },
+}
