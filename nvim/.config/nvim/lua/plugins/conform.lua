fd89bb5bb63d8a045f6e58bdadcfc395af4f848f
darcy <acednes@gmail.com>
2025-11-11 14:22:46 +1100

    nvim: feat(conform): add `superhtml`, `ziggy`, `ziggy_schema`

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -15,6 +15,34 @@ return {
         },
         opts = {
             notify_on_error = false,
+
+            formatters = {
+                superhtml = {
+                    inherit = false,
+                    command = "superhtml",
+                    stdin = true,
+                    args = { "fmt", "--stdin-super" },
+                },
+                ziggy = {
+                    inherit = false,
+                    command = "ziggy",
+                    stdin = true,
+                    args = { "fmt", "--stdin" },
+                },
+                ziggy_schema = {
+                    inherit = false,
+                    command = "ziggy",
+                    stdin = true,
+                    args = { "fmt", "--stdin-schema" },
+                },
+            },
+
+            formatters_by_ft = {
+                shtml = { "superhtml" },
+                xml = { "superhtml" },
+                ziggy = { "ziggy" },
+                ziggy_schema = { "ziggy_schema" },
+            },
         },
     },
 }
