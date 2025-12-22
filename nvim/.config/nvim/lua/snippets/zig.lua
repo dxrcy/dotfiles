054099137ae03497edfed902c66ead5bbc104c64
darcy <acednes@gmail.com>
2025-12-22 18:23:44 +1100

    nvim: feat(snippets/zig): add `importstd` and `importchild`

--- nvim/.config/nvim/lua/snippets/zig.lua
+++ nvim/.config/nvim/lua/snippets/zig.lua
@@ -1,5 +1,12 @@
 local ls = require("luasnip")
 
+--- Reuse text from node.
+local function recall_node(index)
+    return ls.function_node(function(args)
+        return args[1][1]
+    end, { index }, {})
+end
+
 ls.add_snippets("all", {
     ls.snippet("debugprint", {
         ls.text_node([[std.debug.print("]]),
@@ -7,5 +14,19 @@ ls.add_snippets("all", {
         ls.text_node([[\n", .{]]),
         ls.insert_node(2),
         ls.text_node([[});]]),
+    }),
+
+    ls.snippet("importstd", {
+        ls.text_node([[const std = @import("std");]]),
+    }),
+
+    ls.snippet("importchild", {
+        ls.text_node([[const ]]),
+        ls.insert_node(1),
+        ls.text_node([[ = ]]),
+        ls.insert_node(2),
+        ls.text_node([[.]]),
+        recall_node(1),
+        ls.text_node([[;]]),
     }),
 })
