a133bc914579e414eb8730c699b34bd2a7374053
darcy <acednes@gmail.com>
2025-12-22 19:24:59 +1100

    nvim: feat(snippets/zig): add `importfile` and `debugallocator`

--- nvim/.config/nvim/lua/snippets/zig.lua
+++ nvim/.config/nvim/lua/snippets/zig.lua
@@ -8,16 +8,16 @@ local function recall_node(index)
 end
 
 ls.add_snippets("all", {
-    ls.snippet("debugprint", {
-        ls.text_node([[std.debug.print("]]),
-        ls.insert_node(1),
-        ls.text_node([[\n", .{]]),
-        ls.insert_node(2),
-        ls.text_node([[});]]),
-    }),
-
     ls.snippet("importstd", {
         ls.text_node([[const std = @import("std");]]),
+    }),
+
+    ls.snippet("importfile", {
+        ls.text_node([[const ]]),
+        ls.insert_node(1),
+        ls.text_node([[ = @import("]]),
+        recall_node(1),
+        ls.text_node([[.zig");]]),
     }),
 
     ls.snippet("importchild", {
@@ -28,5 +28,21 @@ ls.add_snippets("all", {
         ls.text_node([[.]]),
         recall_node(1),
         ls.text_node([[;]]),
+    }),
+
+    ls.snippet("debugprint", {
+        ls.text_node([[std.debug.print("]]),
+        ls.insert_node(1),
+        ls.text_node([[\n", .{]]),
+        ls.insert_node(2),
+        ls.text_node([[});]]),
+    }),
+
+    ls.snippet("debugallocator", {
+        ls.text_node({
+            [[var gpa = std.heap.DebugAllocator(.{}){};]],
+            [[defer _ = gpa.deinit();]],
+            [[const allocator = gpa.allocator();]],
+        }),
     }),
 })
