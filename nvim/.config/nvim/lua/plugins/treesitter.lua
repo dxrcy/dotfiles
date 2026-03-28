7b6c1de3ea43f73a1220e5d52309afdc4bc64d54
darcy <acednes@gmail.com>
2026-03-28 22:52:59 +1100

    nvim: feat(treesitter): add local `lc3` parser

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,23 +3,33 @@ return { -- Highlight, edit, and navigate code
     branch = "v0.10.0",
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
-    opts = {
-        ensure_installed = {
-            "bash",
-            "cpp",
-            "c",
-            "diff",
-            "html",
-            "lua",
-            "luadoc",
-            "markdown",
-            "markdown_inline",
-            "query",
-            "vim",
-            "vimdoc",
-        },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = false },
-    },
+
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "zig",
+            },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = false },
+        })
+
+        local parser_configs = require("nvim-treesitter.parsers")
+            .get_parser_configs()
+
+        parser_configs.lc3 = {
+            filetype = "lc3",
+            install_info = {
+                url = "~/code/tree-sitter/tree-sitter-lc3",
+                files = { "src/parser.c" },
+            },
+        }
+
+        vim.filetype.add {
+            extension = {
+                asm = "lc3",
+                lc3 = "lc3",
+            },
+        }
+    end,
 }
