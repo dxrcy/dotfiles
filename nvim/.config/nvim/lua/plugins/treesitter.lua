20b359e1f60760307b307d76fcbdcd904c6e54e4
darcy <acednes@gmail.com>
2026-04-09 19:44:21 +1000

    nvim: feat(treesitter): update to `main` branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,41 @@
 return { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
-    branch = "v0.10.0",
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
+    branch = "main",
 
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "zig",
-            },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = false },
+        require("nvim-treesitter").setup()
+
+        -- Manual version of "ensure_installed" and "auto_install" for 'main' branch rewrite
+        require("nvim-treesitter").install({
+            "zig",
         })
 
-        local parser_configs = require("nvim-treesitter.parsers")
-            .get_parser_configs()
+        -- Manual version of "highlight = {enabled = true}" and "indent = {enabled = true}"
+        vim.api.nvim_create_autocmd("FileType", {
+            desc = "Enable treesitter in supported buffers",
+            callback = function()
+                pcall(vim.treesitter.start)
+                -- Use treesitter indentation
+                -- vim.bo.indentexpr = "v:lua require('nvim-treesitter').indentexpr()"
+            end,
+        })
 
-        parser_configs.lc3 = {
-            filetype = "lc3",
-            install_info = {
-                url = "~/code/tree-sitter/tree-sitter-lc3",
-                files = { "src/parser.c" },
-            },
-        }
+        -- TODO:
 
-        vim.filetype.add {
-            extension = {
-                asm = "lc3",
-                lc3 = "lc3",
-            },
-        }
-    end,
+        -- require("nvim-treesitter.parsers").lc3 = {
+        --     filetype = "lc3",
+        --     install_info = {
+        --         url = "~/code/tree-sitter/tree-sitter-lc3",
+        --         files = { "src/parser.c" },
+        --     },
+        -- }
+        --
+        -- vim.filetype.add {
+        --     extension = {
+        --         asm = "lc3",
+        --         lc3 = "lc3",
+        --     },
+        -- }
+    end
+
 }
