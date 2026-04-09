82e1c1dd188f717bf820afd7d25658b9eb2337b9
darcy <acednes@gmail.com>
2026-04-09 20:17:27 +1000

    nvim: fix(treesitter): add `build` option

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
+    build = ":TSUpdate",
 
     config = function()
         require("nvim-treesitter").setup()
