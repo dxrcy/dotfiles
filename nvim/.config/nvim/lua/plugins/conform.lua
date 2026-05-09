81e870579104985514a54d3dafd0c906f2a87df8
darcy <acednes@gmail.com>
2026-05-09 21:39:22 +1000

    feat(nvim/conform): add `python`

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -17,6 +17,7 @@ return { -- Formatter
             javascript = { "prettier" },
             typescript = { "prettier" },
             json = { "prettier" },
+            python = { "black" },
         },
     },
 }
