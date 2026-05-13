a1dcd73c7aa2b5f79f2340a2db4abd90a9550e96
darcy <acednes@gmail.com>
2026-05-13 12:13:56 +1000

    feat(nvim): swap `mini-statusline` with `lualine`

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,29 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependecies = { "nvim-mini/mini.icons" },
+
+    config = function()
+        require("lualine").setup({
+            options = {
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+                always_divide_middle = false,
+                disabled_filetypes = {
+                    "neo-tree",
+                },
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "filename" },
+                lualine_x = {
+                    "notebook",
+                    "location",
+                },
+                lualine_y = { "filetype" },
+                lualine_z = {},
+            },
+        })
+    end,
+}
