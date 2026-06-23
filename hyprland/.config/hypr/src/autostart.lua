42bc0717d12717de0f7f56d79a24fbd2d5dc7c94
darcy <acednes@gmail.com>
2026-06-23 21:53:24 +1000

    refactor(hypr): use `hl.timer` instead of `exec_cmd` with `sleep`

--- hyprland/.config/hypr/src/autostart.lua
+++ hyprland/.config/hypr/src/autostart.lua
@@ -15,13 +15,19 @@ return function()
 	hl.exec_cmd("hyprsunset")
 	hl.exec_cmd("wvkbd-mobintl --hidden -L 250")
 
-	hl.exec_cmd("sleep 1; bt mute-if-disconnected")
-	hl.exec_cmd("sleep 1; volume-brightness.nu microphones disable")
+	hl.timer(function()
+		hl.exec_cmd("bt mute-if-disconnected")
+		hl.exec_cmd("volume-brightness.nu microphones disable")
+	end, { type = "oneshot", timeout = 1000 })
 
 	hl.exec_cmd(root.browser, { workspace = "1 silent" })
 
-	-- TODO: Use `hl.timer`
-	hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland & sleep 2 & /usr/lib/xdg-desktop-portal")
+	hl.dispatch(function()
+		hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland")
+		hl.timer(function()
+			hl.exec_cmd("/usr/lib/xdg-desktop-portal")
+		end, { type = "oneshot", timeout = 2000 })
+	end)
 
 	hl.dispatch(hl.dsp.focus { workspace = 2 })
 
