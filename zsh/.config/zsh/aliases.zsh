56ac85d1e59966ef415e1433b2da15a1a456894c
darcy <acednes@gmail.com>
2026-07-27 14:29:36 +1000

    fix(zsh): fix `ls` alias args

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -38,7 +38,7 @@ alias journalctl='journalctl -e'
 alias slop='opencode . -c'
 
 # ls (eza) options
-alias ls='eza -l --group-directories-first --icons'
+alias ls='eza -l --group-directories-first --icons=auto'
 alias lsa='ls -a'
 alias lst='ls --total-size --sort size --reverse'
 
