4dce90c408b1baacecda6eed15475c62310c4880
darcy <acednes@gmail.com>
2026-08-14 12:14:46 +1000

    fix(hypr): disable `non_consuming` for kb switch

--- hyprland/.config/hypr/src/binds.lua
+++ hyprland/.config/hypr/src/binds.lua
@@ -210,7 +210,7 @@ local function switch_kb_layout()
 	local kb_layout = hl.get_config("input.kb_layout") ~= "us" and "us" or "epo"
 	hl.config { input = { kb_layout = kb_layout } }
 end
-bind2({ "ALT", "Q" }, switch_kb_layout, { non_consuming = true })
+bind2({ "ALT", "Q" }, switch_kb_layout)
 
 bind({ "print" }, hl.dsp.exec_cmd("flameshot gui"))
 bind({ "SHIFT", "print" }, hl.dsp.exec_cmd('grim -g "$(slurp -d)" - | wl-copy'))
