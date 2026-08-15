aca127f1b029e4fd6d4031df1834bdbe1634e9c9
darcy <acednes@gmail.com>
2026-08-15 20:38:21 +1000

    feat(hypr): use `scrolling` layout

--- hyprland/.config/hypr/src/variables.lua
+++ hyprland/.config/hypr/src/variables.lua
@@ -11,7 +11,7 @@ hl.config {
 			active_border = 0xff9b8921,
 		},
 
-		layout = "master",
+		layout = "scrolling",
 		no_focus_fallback = true,
 
 		resize_on_border = true,
@@ -23,6 +23,18 @@ hl.config {
 	master = {
 		mfact = 0.6,
 		new_status = "master",
+	},
+
+	scrolling = {
+		-- fullscreen_on_one_column = false,
+		-- column_width = 0.5,
+		-- focus_fit_method = 1,
+		-- follow_focus = true,
+		-- follow_min_visible = 0.4,
+		-- explicit_column_widths = "0.333, 0.5, 0.667, 1.0",
+		-- wrap_focus = true,
+		-- wrap_swapcol = true,
+		-- direction = "right",
 	},
 
 	decoration = {
