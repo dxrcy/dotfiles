d13caaf64d4744cf6d97cb96918f87faaa886ab0
darcy <acednes@gmail.com>
2026-08-17 14:15:51 +1000

    feat(nvim): dim inactive code instead of showing as comment

--- nvim/.config/nvim/lua/plugins/kanso.lua
+++ nvim/.config/nvim/lua/plugins/kanso.lua
@@ -6,5 +6,13 @@ return { -- Colorscheme selection
 	end,
 	opts = {
 		transparent = true,
+		overrides = function(_)
+			return {
+				-- Inactive code should be dimmed, not greyed-out like a comment
+				DiagnosticUnnecessary = { dim = true },
+				["@lsp.type.comment.c"] = { link = "DiagnosticUnnecessary" },
+				["@lsp.type.comment.cpp"] = { link = "DiagnosticUnnecessary" },
+			}
+		end
 	},
 }
