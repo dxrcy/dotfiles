7da053b3eb95578ac70babdf8d287b296d8a0526
darcy <acednes@gmail.com>
2026-08-19 21:11:34 +1000

    fix(hypr): support varargs for `log`

--- hyprland/.config/hypr/src/logging.lua
+++ hyprland/.config/hypr/src/logging.lua
@@ -4,8 +4,8 @@ local FILEPATH = "/tmp/hypr.log"
 
 ---@param level "info"|"warn"|"err"
 ---@param scopes string[]
----@param message string
-M.log = function(level, scopes, message)
+---@param message ...string a
+M.log = function(level, scopes, ...message)
 	local name = ""
 	for _, scope in ipairs(scopes) do
 		if #name > 1 then name = name .. "." end
@@ -17,7 +17,13 @@ M.log = function(level, scopes, message)
 	line = line .. os.date("%Y-%m-%dT%H:%M:%S")
 	line = line .. " " .. level:upper()
 	line = line .. name
-	line = line .. ": " .. tostring(message)
+	line = line .. ": "
+
+	for i = 1, #message do
+		if i > 1 then line = line .. " " end
+		line = line .. tostring(message[i])
+	end
+
 
 	local file, _ = io.open(FILEPATH, "a")
 	if file then
