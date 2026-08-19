70d1285447eeac20ff4fafd68379a83f0ea908cc
darcy <acednes@gmail.com>
2026-08-19 12:08:19 +1000

    feat(hypr): change scrolling layout column directino

--- hyprland/.config/hypr/src/variables.lua
+++ hyprland/.config/hypr/src/variables.lua
@@ -27,14 +27,14 @@ hl.config {
 
 	scrolling = {
 		-- fullscreen_on_one_column = false,
-		-- column_width = 0.5,
+		column_width = 0.6,
 		-- focus_fit_method = 1,
 		-- follow_focus = true,
 		-- follow_min_visible = 0.4,
 		-- explicit_column_widths = "0.333, 0.5, 0.667, 1.0",
 		-- wrap_focus = true,
 		-- wrap_swapcol = true,
-		-- direction = "right",
+		direction = "left",
 	},
 
 	decoration = {
